28602d145c5249d1feb8a2813a429ddc43211c2c
Isaac Chan <isaac.chan13@gmail.com>
2017-10-08 20:52:05 -0700

    add mutexes and rand time to repo master

--- hw1/hw1.c
+++ hw1/hw1.c
@@ -8,13 +8,25 @@ _Bool SupportsRDRAND; //true if supported, false if not supported
 pthread_t prod;
 pthread_t cons;
 
-//TODO: some type of queue structure for the event buffer
+//init rand num gen
+srand((unsigned) time(&t));
+
+//blocks changes being made to buffer
+pthread_mutex_t busy_mutex;
+
+//blocks consumers if the buffer is empty
+pthread_mutex_t empty_mutex;
+
+//blocks producers if the buffer is full
+pthread_mutex_t full_mutex;
 
 struct buffer_vals{
 	int num;
 	int rand_time;
 	};
 
+struct buffer_vals buffer[32];
+pthread_mutex_t mutex;
 
 //https://codereview.stackexchange.com/questions/147656/checking-if-cpu-supports-rdrand
 _Bool SupportsRDRAND(){
@@ -22,6 +34,22 @@ _Bool SupportsRDRAND(){
 	unsigned int eax, ebx, ecx, edx;
 	__get_cpuid(1, &eax, &ebx, &ecx, &edx);
 	return ((ecx & flag_RDRAND) == flag_RDRAND);
+}
+
+int gen_rand(int min, int max){
+   	unsigned int value;
+	if(!supports_rdrand){
+		value = (rand() % (max + 1 - min)) + min;
+	}
+	else{
+		__asm__("rdrand  %[value]"
+			: [value] "=r" (value)
+			:       /* no inputs */
+			: "cc"  /* clobbers flags (condition codes) */
+			);
+		value = (value % (max + 1 - min)) + min;
+	}
+	return value;
 }
 
 int init_rand(){
@@ -33,11 +61,18 @@ int init_rand(){
 
 void *producer(void *param){
 	//init a new event
+	struct buffer_vals *new_val;
+
 	//fill event (with random number (srand function in stdlib))
-	//see if the queue is full
-	//see if the queue is busy (waiting time)
-	//put the event into the buffer with random wait time
-	//increment the semaphore	
+	new_val = malloc(sizeof(struct buffer));
+	new_val->num = gen_rand(0,5);
+	new_val->rand_time = gen_rand(2,9);	
+	
+	//see if the queue is full (update state and wait for state for state to change)
+	
+	//see if the queue is busy (waiting time, update state?)
+	//put the event into the buffer with random wait time (3-7)
+	int wait = gen_rand(3, 7);
 }
 
 void *consumer(void *param){
@@ -56,7 +91,5 @@ int main(){
 	//placeholders
 	pthread_create(&prod, NULL, *producer, NULL);
 	pthread_create(&cons, NULL, *consumer, NULL);
-	
-	return 0;
 }
 
